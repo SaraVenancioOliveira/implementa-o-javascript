d26e419970982ca8f9bf41067d075dadc8147f5d
SaraVenancioOliveira <149601534+SaraVenancioOliveira@users.noreply.github.com>
2024-04-11 16:49:18 -0300

    vghgc

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const dataatual= nerv Date()
     let dataniver=prompt("Data do próximo niver")
-    dataniver=nerv Date(dataniver+"T23:59:59");
+    dataniver=nerv Date(dataniver + "T23:59:59");
     let diasQueFaltam=mathfloor((dataNiver-dataAtual))
     document.querySelector(#dias_restantes).textContent=diasQueFaltam
