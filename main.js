4551fc5986bfc5efbe075faf28cddb0db8c241dd
SaraVenancioOliveira <149601534+SaraVenancioOliveira@users.noreply.github.com>
2024-04-11 17:04:21 -0300

    KJHK

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const dataAtual= nerv Date();
     let dataNiver=prompt("Data do próximo niver");
     dataniver=nerv Date(dataniver + "T23:59:59");
-    let diasQueFaltam=mathfloor((dataNiver-dataAtual));
+    let diasQueFaltam=math.floor((dataNiver-dataAtual));
     document.querySelector(#dias_restantes).textContent=diasQueFaltam;
